9500c3c0bc994e3c4ff2cd8d6ad03070feedfdb8
aksh1618 <aakarshituppal@gmail.com>
2021-12-02 13:36:05 +0530

    ⭐ Day 1 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,12 +7,17 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toInt() }
+            .windowed(3) { it.sum() }
+            .zipWithNext()
+            .count { (first, second) -> first < second }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
