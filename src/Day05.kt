0b67b2f661a2b6a47f27c3bb88d2621680c0d0e4
aksh1618 <aakarshituppal@gmail.com>
2021-12-06 02:05:18 +0530

    ⭐ Day 5 Part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -11,6 +11,18 @@ fun LineSegment.points(): List<Point> {
     val points = mutableListOf<Point>()
     if (x1 == x2) (min(y1, y2)..max(y1, y2)).forEach { points += x1 to it }
     else if (y1 == y2) (min(x1, x2)..max(x1, x2)).forEach { points += it to y1 }
+    else {
+        points += x1 to y1
+        var i = x1
+        var j = y1
+        while (i != x2 && j != y2) {
+            if (i > x2) i--
+            else if (i < x2) i++
+            if (j > y2) j--
+            else if (j < y2) j++
+            points += i to j
+        }
+    }
     return points
 }
 
@@ -24,8 +36,12 @@ fun main() {
         }
     }
 
-    fun part1(lineSegments: List<LineSegment>): Int {
+    fun intersectionsForLinesSatisfying(
+        lineSegments: List<LineSegment>,
+        predicate: (LineSegment) -> Boolean,
+    ): Int {
         return lineSegments
+            .filter(predicate)
             .fold(mutableMapOf<Point, Int>()) { map, lineSegment ->
                 map.apply {
                     lineSegment.points().forEach { put(it, (getOrPut(it) { 0 }) + 1) }
@@ -34,13 +50,20 @@ fun main() {
             .count { it.value >= 2 }
     }
 
+    fun part1(lineSegments: List<LineSegment>): Int {
+        return intersectionsForLinesSatisfying(lineSegments) { (point1, point2) ->
+            point1.first == point2.first || point1.second == point2.second
+        }
+    }
+
     fun part2(lineSegments: List<LineSegment>): Int {
-        return lineSegments.size
+        return intersectionsForLinesSatisfying(lineSegments) { true }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readLineSegments(readInput("Day05_test"))
     check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
 
     val input = readLineSegments(readInput("Day05"))
     println(part1(input))
